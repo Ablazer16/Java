c8e048c395243ce531a4b46e306a9547ba50002f
Adarsh Kumar <adarsh16022003@gmail.com>
2022-11-20 15:58:54 +0530

    Floor

--- class/CelingNumber.java
+++ class/CelingNumber.java
@@ -1,12 +1,15 @@
 public class CelingNumber {
     public static void main(String[] args) {
-        int arr[] = {2, 4, 6, 14, 22, 56, 83, 75};
+        int arr[] = {2, 4, 6, 14, 22, 56, 63, 85};
         int celing = 81;
         int ans = binarySearch(arr, celing);
         System.out.println(ans);
     }
 
     static int binarySearch(int arr[], int celing){
+        if(celing>arr[arr.length-1]){
+            return -1;
+        }
         int start = 0;
         int end = arr.length - 1;
 
